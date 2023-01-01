6a8d04181d16489161a526121b5d5b20828356df
narendraganjikunta <108442254+narendraganjikunta@users.noreply.github.com>
2023-01-01 18:54:14 +0530

    first commit

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './Footer.css'
+function Footer() {
+  return (
+    <footer className="bg-dark text-center text-white">
+        <div class="text-center p-3">
+    © 2020 Copyright:
+    <a className="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+  </div>
+</footer>
+  )
+}
+
+export default Footer
